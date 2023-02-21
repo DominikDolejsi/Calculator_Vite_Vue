5117d88ab1f0ed4b148752ae351f9bd6e6ba9faa
Dominik Dolejší <bukefalus007@gmail.com>
2023-02-21 12:47:05 +0100

    Delete old lines in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
 import Calc from "./components/Calculator.vue"
-import Reactivity from "./components/Reactivity.vue"
 </script>
 
 <template>
   <Calc />
-  <!-- <Reactivity /> -->
 </template>
 
 <style scoped>
